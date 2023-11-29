2831d38d40290ee71bc5d33fba6606ae70d40a07
Charles Clinton Pustejovsky III <charles@cpustejovsky.com>
2023-11-29 18:46:28 -0500

    feat: minor refactors to Calculate
    move valdiating input to its own function

--- domain/lifetogether/calculator.go
+++ domain/lifetogether/calculator.go
@@ -30,24 +30,16 @@ type Output struct {
 
 //TODO: create error structs to store information
 
+// CalculateNow takes Input and returns an Output and error based on the current time
 func CalculateNow(in Input) (*Output, error) {
 	return Calculate(time.Now(), in)
 }
 
-// Calculate takes Input and return a pointer to Output along with an error
+// Calculate takes Input and return a pointer to Output and error based on the provided time
 func Calculate(t time.Time, in Input) (*Output, error) {
-	//Validate Mandatory Parameters
-	if in.YourName == "" {
-		return nil, errors.New("provide your name")
-	}
-
-	if in.OtherName == "" {
-		return nil, errors.New("provide other person's name")
-	}
-
-	// Validate that DateMet is greater than both YourBirthday and OtherBirthday
-	if in.DateMet.Before(in.YourBirthday) || in.DateMet.Before(in.OtherBirthday) {
-		return nil, errors.New("the date you both met should be after you both were born")
+	err := validateInput(in)
+	if validateInput(in) != nil {
+		return nil, err
 	}
 
 	var out = Output{
@@ -62,7 +54,6 @@ func Calculate(t time.Time, in Input) (*Output, error) {
 	}
 	out.MetDuration = md
 	//Calculate percentage person and other have known each other
-	metDurationFloat := float64(md)
 	yb, err := calculateDayDuration(t, in.YourBirthday)
 	if err != nil {
 		return nil, fmt.Errorf("your birthday error %w", err)
@@ -73,28 +64,42 @@ func Calculate(t time.Time, in Input) (*Output, error) {
 	}
 	yourAlive := float64(yb)
 	otherAlive := float64(ob)
+	metDurationFloat := float64(md)
 	out.YourPercentTogether = round(metDurationFloat/yourAlive*100, 2)
 	out.OtherPercentTogether = round(metDurationFloat/otherAlive*100, 2)
 
 	//Calculate for optional parameters
-	dd, err := calculateDayDuration(t, *in.DateDating)
+	dating, err := calculateDayDuration(t, *in.DateDating)
 	if err != nil {
 		out.DatingDuration = nil
 	} else {
-		out.DatingDuration = &dd
+		out.DatingDuration = &dating
 	}
-	md, err = calculateDayDuration(t, *in.DateMarried)
+	married, err := calculateDayDuration(t, *in.DateMarried)
 	if err != nil {
 		out.MarriedDuration = nil
 	} else {
-		out.MarriedDuration = &md
+		out.MarriedDuration = &married
 	}
 	return &out, nil
 }
 
-func round(num float64, decimals int) float64 {
-	precision := math.Pow10(decimals)
-	return math.Round(num*precision) / precision
+func validateInput(in Input) error {
+	//Validate Mandatory Parameters
+	if in.YourName == "" {
+		return errors.New("provide your name")
+	}
+
+	if in.OtherName == "" {
+		return errors.New("provide other person's name")
+	}
+
+	// Validate that DateMet is greater than both YourBirthday and OtherBirthday
+	if in.DateMet.Before(in.YourBirthday) || in.DateMet.Before(in.OtherBirthday) {
+		return errors.New("the date you both met should be after you both were born")
+	}
+
+	return nil
 }
 
 func calculateDayDuration(t time.Time, dur time.Time) (int, error) {
@@ -104,4 +109,9 @@ func calculateDayDuration(t time.Time, dur time.Time) (int, error) {
 	hoursDur := t.Sub(dur).Hours()
 	days := math.Floor(hoursDur / 24.0)
 	return int(days), nil
+}
+
+func round(num float64, decimals int) float64 {
+	precision := math.Pow10(decimals)
+	return math.Round(num*precision) / precision
 }
