a84776cc04dd23f8438aa84fba59923ed32496d0
cpustejovsky <charles@cpustejovsky.com>
2024-08-12 14:11:42 -0400

    Have resource page read a html file in repo

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 sass --no-source-map handlers/static/styles.scss:handlers/static/styles.min.css --style compressed
-
+pandoc -f markdown -t html ./handlers/static/resources.md -o ./handlers/static/resources.html
 go run ./cmd/server/main.go
