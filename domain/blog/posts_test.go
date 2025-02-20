11f92c56a8c01e5c86fa20bf4f5cc89170b3cf0d
Charles Clinton Pustejovsky III <charles@cpustejovsky.com>
2025-02-20 13:34:17 -0500

    Fix OrderedPosts function call

--- domain/blog/posts_test.go
+++ domain/blog/posts_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestAll(t *testing.T) {
 	t.Log(blog.AllPosts)
-	op := blog.OrderedPosts(blog.AllPosts...)
+	op := blog.OrderedPosts(blog.AllPosts)
 	t.Log(op)
 	for _, post := range op {
 		t.Log(post.HumanReadableDate())
