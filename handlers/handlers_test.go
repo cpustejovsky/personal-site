27f35063f4f4860c4e3665f32f1fe5812d602018
Charles Clinton Pustejovsky III <charles@cpustejovsky.com>
2025-02-20 13:34:49 -0500

    Fix GetResourcePage code to use embedded static file

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -1,20 +1,15 @@
 package handlers_test
 
 import (
-	"os"
 	"testing"
 
 	"github.com/cpustejovsky/personal-site/handlers"
 )
 
 func TestGetResourcesPage(t *testing.T) {
-	wd, err := os.Getwd()
+	data, err := handlers.GetResourcesPage()
 	if err != nil {
 		t.Fatal(err)
 	}
-	wd += "/static/resources.html"
-	_, err = handlers.GetResourcesPage(wd)
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Log("data", data)
 }
